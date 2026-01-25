8eb1d1f3ca2e22f22e5297f59c0ef4ba71f33bca
Sara AbuHimed <sara.a.abuhimed@gmail.com>
2026-01-25 12:17:57 +0300

    Add build_visualization_planner_graph

--- src/langgraph/workflow.py
+++ src/langgraph/workflow.py
@@ -1,2 +1,20 @@
-# from langgraph.graph import StateGraph, START, END
-# from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import StateGraph, START, END
+from langgraph.checkpoint.memory import MemorySaver
+
+from .state import VizPlannerState
+from .nodes import visualization_planner_node
+
+
+def build_visualization_planner_graph():
+    graph = StateGraph(VizPlannerState)
+
+    graph.add_node("viz_planner", visualization_planner_node)
+    graph.add_edge(START, "viz_planner")
+    graph.add_edge("viz_planner", END)
+
+    # MemorySaver is good for dev/testing and chaining
+    return graph.compile(checkpointer=MemorySaver())
+
+
+# Export a ready-to-use compiled graph object
+viz_planner_graph = build_visualization_planner_graph()
